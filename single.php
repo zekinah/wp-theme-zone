840768a639e234cbb2689a07e83d394f16a786f5
zekinah <zjlecaros@gmail.com>
2020-09-11 21:47:15 -0700

    Small Adjustment

--- single.php
+++ single.php
@@ -14,27 +14,30 @@ $container = get_theme_mod( 'themezone_container_type' );
 <div class="wrapper" id="archive-wrapper">
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 		<div class="row">
-			<main id="primary" class="site-main">
-				<?php
-				while ( have_posts() ) :
-					the_post();
+			<div class="col-md-8 content-area" id="primary">
+				<main id="primary" class="site-main">
+					<?php
+					while ( have_posts() ) :
+						the_post();
 
-					get_template_part( 'template-parts/contents/content', get_post_type() );
+						get_template_part( 'template-parts/contents/content', get_post_type() );
 
-					the_post_navigation(
-						array(
-							'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'themezone' ) . '</span> <span class="nav-title">%title</span>',
-							'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'themezone' ) . '</span> <span class="nav-title">%title</span>',
-						)
-					);
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
+						the_post_navigation(
+							array(
+								'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'themezone' ) . '</span> <span class="nav-title">%title</span>',
+								'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'themezone' ) . '</span> <span class="nav-title">%title</span>',
+							)
+						);
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
 
-				endwhile; // End of the loop.
-				?>
-			</main><!-- #main -->
+					endwhile; // End of the loop.
+					?>
+				</main><!-- #main -->
+			</div>
+			<?php get_sidebar(); ?>
 		</div><!-- .row -->
 	</div><!-- #content -->
 </div><!-- #archive-wrapper -->
