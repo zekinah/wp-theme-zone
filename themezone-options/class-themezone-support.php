dfb349924b353a9cf854e9c8a29fe64ed5002157
zekinah <zjlecaros@gmail.com>
2020-09-18 21:55:12 -0700

    Added Container of released details for support tab

--- themezone-options/class-themezone-support.php
+++ themezone-options/class-themezone-support.php
@@ -52,7 +52,60 @@ if ( ! class_exists( 'Themezone_Support' ) ){
         }
 
         public function zone_option_support() {
+            $updates = $this->zone_option_support_changes();
             require THEME_ZONE_URI . '/templates/support.php';
+        }
+
+        public function zone_option_support_changes() {
+            $updates = array();
+
+            $updates = array(
+                array(
+                    'title' => 'Initial Released',
+                    'version' => '1.0.0',
+                    'type' => 'system-update',
+                    'details' => array(
+                        'desc' => '',
+                        'sect' => array(
+                            array(
+                                'func_name' => 'Global',
+                                'funtions' => array(
+                                    'General // Layout',
+                                    'Copyrights // content',
+                                    'Hooks // Can Add custom SEO scripts'
+                                )
+                            ),
+                            array(
+                                'func_name' => 'Social',
+                                'funtions' => array(
+                                    'General // Social Media links'
+                                )
+                            ),
+                            array(
+                                'func_name' => 'Colors',
+                                'funtions' => array(
+                                    'General // Background Color of header, body, footer'
+                                )
+                            ),
+                            array(
+                                'func_name' => 'Addons',
+                                'funtions' => array(
+                                    'General // Options to use embeded third party libraries'
+                                )
+                            ),
+                            array(
+                                'func_name' => 'Custom CSS & JS',
+                                'funtions' => array(
+                                    'CSS // Section to add custom CSS',
+                                    'JS // Section to add custom JS'
+                                )
+                            ),
+                        )
+                    )
+                ),
+            );
+
+            return $updates;
         }
 
     
